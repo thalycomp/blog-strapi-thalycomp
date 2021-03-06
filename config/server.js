39fe113db4df00b9b08baffbaf9b421829de3f06
thalyta <thalycomp@gmail.com>
2021-03-06 16:26:24 -0300

    iniciando strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bef46a80eb1517887e057638f5b0407b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '283d33a8-3bc6-4257-9081-de756de4d536'
+};
