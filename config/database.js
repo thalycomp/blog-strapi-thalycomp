358c940fb8e07a8ed89fa3584c4ac5ee9205181b
thalyta <thalycomp@gmail.com>
2021-03-10 14:50:19 -0300

    atualizando

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-module.exports = ({ env }) => ({
+/*module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
@@ -15,4 +15,31 @@ module.exports = ({ env }) => ({
       options: {}
     },
   },
-});
+});*/
+
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "postgres",
+          host,
+          port,
+          database,
+          username: user,
+          password,
+          ssl: { rejectUnauthorized: false }
+        },
+        options: {
+          ssl: false
+        },
+      },
+    },
+  };
+};
