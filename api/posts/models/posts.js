42392f7e0129808ee8aaef8f700fc03fadfe6dfe
thalyta <thalycomp@gmail.com>
2021-03-30 14:22:49 -0300

    adicionando lifecycles

--- api/posts/models/posts.js
+++ api/posts/models/posts.js
@@ -5,4 +5,13 @@
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    async afterCreate(result, data) {
+      axios.post('https://api.netlify.com/build_hooks/60635e4c841948389fbf28ea');
+    },
+    async afterUpdate(result, params, data) {
+      axios.post('https://api.netlify.com/build_hooks/60635e4c841948389fbf28ea');
+    },
+  }
+};
