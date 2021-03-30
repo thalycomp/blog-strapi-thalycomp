886ec79970a5ad8319b38f4e2a86f3ee6d70d9e8
thalyta <thalycomp@gmail.com>
2021-03-30 14:35:36 -0300

    corrigindo models/posts

--- api/posts/models/posts.js
+++ api/posts/models/posts.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const axios = require('axios');
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/models.html#lifecycle-hooks)
  * to customize this model
